13707d81f8b36831b193391717a6ff083f36acd8
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-02-27 16:31:57 -0600

    ARGGGG

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var path = require('path');
 var port = 3000;
 
 
 // initial jokes provided by the client
+//package as object
 var jokes = [
   {
     whoseJoke: "Luke",
@@ -23,14 +25,29 @@ var jokes = [
   }
 ];
 
+
 // static file requests
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // routes
+app.get('/jokes', function(req, res){
+  res.send(jokes);
+});
+//Set up server side routes to handle requests
+app.post('/jokes/new', function(req, res){
+  console.log('in jokes post:', jokes);
+res.send(jokes);
+
+
+}); //end /jokes post
+
+
 
 
 // Send index.html file
 app.get('/', function(req, res) {
+  console.log('home base url hit');
   res.sendFile(path.resolve('server/public/views/index.html'));
 });
 
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function(){
   console.log('jquery loaded');
 
+
+// Send requests to the server using ajax
+$.ajax({
+  type: 'GET',
+  url: '/jokes',
+  success: function(response) {
+    console.log('response', response); // returns fis
+    }
 });
+ //
+
+
+// Display response on the DOM
+getJokeData();
+function getJokeData(){
+  $.ajax({
+    type: 'POST',
+    url: '/jokes/new',
+    success: function(response) {
+      console.log('response', response); // returns jokeList array
+      $('#jokeContainer').empty();
+      for (var i = 0; i < response.length; i++) { //response is the array
+        $('#jokeContainer').append('<li>' + response[i].whoseJoke + ': ' + response[i].jokeQuestion + ' punchline: ' + response[i].punchLine + '</li>'); //append name to li in DOM
+      }
+    }
+  });
+}
+
+
+// $('#newJokeButton').on('click', function(){
+//     var newJokeObject = {};
+//     newJokeObject.whoseJoke = $('whoseJoke').val();
+//     $.ajax({
+//       type: 'POST',
+//       url: '/jokes/new/new',
+//       data: newJokeObject,
+//       success: function(response) {
+//         console.log(response);
+//         getJokeData();
+//       },
+//       error: function(error){
+//       alert('ERROR: Please Enter A New NAME!');
+//     }
+//     });
+//   });
+
+});//end doc ready
